816d6e435da8c5822990bebdaaa0e7f489aa51e6
DarkStar <darkstar@unisocial.net>
2024-10-14 17:42:14 +0300

    0

--- src/backup.sh
+++ src/backup.sh
@@ -4,3 +4,6 @@ mkdir -p podman/root/{var.cache,etc}
 
 sudo rsync -av --progress /etc/apt ${package}/root/etc/
 sudo rsync -av --progress /var/cache/apt ${package}/root/var.cache/
+
+sudo chown -R $USER:$USER $package
+sudo chmod -R 744 $package
